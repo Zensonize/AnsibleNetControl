a292d48334ef1c60d9748495572b2eefa5ee2970
zensonize <krittametisworking@gmail.com>
2020-07-28 20:13:42 +0700

    finished dashboard page

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -1,9 +1,12 @@
 <template>
     <div id="dashboard">
         <QuickInfo/>
-        <h1>Hello this is Dashboard</h1>
-
-        {{$store.state.hosts}}
+        <!-- <h1>Hello this is Dashboard</h1> -->
+        <div class="container">
+            <GroupTable v-for="(groups, index) in $store.state.hosts.hosts" v-bind:key="index" v-bind:index="index" v-bind:groupName="index" v-bind:hostList="$store.state.hosts.hosts[index]"/>
+        </div>
+        
+        <!-- {{$store.state.hosts}} -->
     </div>
 </template>
 
@@ -11,6 +14,7 @@
     import {mapActions} from 'vuex'
 
     import QuickInfo from './dashboard/QuickInfo.vue'
+    import GroupTable from './dashboard/GroupTable'
 
     export default {
         created() {
@@ -20,7 +24,8 @@
             'initHosts'
         ]),
         components: {
-            QuickInfo
+            QuickInfo,
+            GroupTable
         }
     }
 </script>
---
+++ frontend/src/components/dashboard/GroupTable.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="group-table" v-show="!groupName.includes('localhost')">
+        <h1>{{groupName}}</h1>
+        <HostTable v-for="(host,index) in hostList.hosts" v-bind:key="index" v-bind:hostName="host" v-bind:hostInformation="hostList[host]"/>
+    </div>
+</template>
+
+<script>
+import HostTable from './HostTable'
+
+    export default {
+        props: ['groupName','hostList','index'],
+        data() {
+            return {
+                msg: "test"
+            }
+        },
+        components: {
+            HostTable
+        }
+    }
+</script>
